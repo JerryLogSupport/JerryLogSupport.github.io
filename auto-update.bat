adbffd2e7a8a2eb1a2f86a741734de77b6736b7b
JerryLogSupport <support@jerry-log.com>
2026-03-02 15:03:00 +0800

    Delete auto-update.bat

--- auto-update.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM
-cd /d "d:\Documents\GitHub\JerryLogSupport.github.io"   ← 把这里改成你的仓库实际路径
-git add --all
-git commit -m "自动更新 Excel - %date% %time%" || exit /b 0   REM 如果没变化就跳过
-git push origin main
-echo 更新完成！
-pause
