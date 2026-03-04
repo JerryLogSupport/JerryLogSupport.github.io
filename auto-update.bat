745637bc20cf015821984f7b44e9c76aabdc8175
JerryLogSupport <support@jerry-log.com>
2026-03-04 17:25:15 +0800

    自动更新 Excel - 2026/03/04 周三 17:25:15.09

--- auto-update.bat
+++ auto-update.bat
@@ -1,6 +1,5 @@
 @echo off
-REM
-cd /d "d:\Documents\GitHub\JerryLogSupport.github.io"   ← 把这里改成你的仓库实际路径
+cd /d "d:\Documents\GitHub\JerryLogSupport.github.io"   REM← 把这里改成你的仓库实际路径
 git add --all
 git commit -m "自动更新 Excel - %date% %time%" || exit /b 0   REM 如果没变化就跳过
 git push origin main
